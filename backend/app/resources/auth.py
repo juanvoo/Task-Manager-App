4342d5768fa2e4a91a79ff471b60cc08d1639745
Juan Moreira <juanmoreiracosta2016@gmail.com>
2025-07-24 20:27:22 -0300

    Validación de formulario

--- backend/app/resources/auth.py
+++ backend/app/resources/auth.py
@@ -2,4 +2,5 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import create_access_token, create_refresh_token
 from app import db
 from app.models.user import User
+from app.utils.validators import validate_email
 
